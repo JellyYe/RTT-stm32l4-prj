200a07a7566633b1a7644900165656d809f94c33
shipsof <984490128@qq.com>
2019-08-12 16:44:58 +0800

    修改程序

--- rt-thread/bsp/stm32/stm32l475-atk-pandora/applications/main.c
+++ rt-thread/bsp/stm32/stm32l475-atk-pandora/applications/main.c
@@ -316,22 +316,22 @@ int main( void )
 //					     1024, 6, 10 );
 //	if ( DFS_thread != RT_NULL )
 //		rt_thread_startup( DFS_thread );
-	    extern rt_err_t ping(char* target_name, rt_uint32_t times, rt_size_t size);
-    
-    /* 等待网络连接成功 */
-    rt_thread_mdelay(500);
-    
-    while(1)
-    {
-        if(ping("www.rt-thread.org", 4, 0) != RT_EOK)
-        {
-            rt_thread_mdelay(5000);
-        }
-        else
-        {
-            break;
-        }
-    }
+//	    extern rt_err_t ping(char* target_name, rt_uint32_t times, rt_size_t size);
+//    
+//    /* 等待网络连接成功 */
+//    rt_thread_mdelay(500);
+//    
+//    while(1)
+//    {
+//        if(ping("www.rt-thread.org", 4, 0) != RT_EOK)
+//        {
+//            rt_thread_mdelay(5000);
+//        }
+//        else
+//        {
+//            break;
+//        }
+//    }
 	
 	led_blink();
 	    
