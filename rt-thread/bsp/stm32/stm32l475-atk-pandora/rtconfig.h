7fd6c9e17ca4e4ec32ffc4dcaca8717432adf146
shipsof <984490128@qq.com>
2019-09-27 10:53:54 +0800

    更新rt笔记

--- rt-thread/bsp/stm32/stm32l475-atk-pandora/rtconfig.h
+++ rt-thread/bsp/stm32/stm32l475-atk-pandora/rtconfig.h
@@ -79,18 +79,6 @@
 #define DFS_FILESYSTEMS_MAX 16
 #define DFS_FILESYSTEM_TYPES_MAX 16
 #define DFS_FD_MAX 64
-#define RT_USING_DFS_ELMFAT
-
-/* elm-chan's FatFs, Generic FAT Filesystem Module */
-
-#define RT_DFS_ELM_CODE_PAGE 437
-#define RT_DFS_ELM_WORD_ACCESS
-#define RT_DFS_ELM_USE_LFN_3
-#define RT_DFS_ELM_USE_LFN 3
-#define RT_DFS_ELM_MAX_LFN 255
-#define RT_DFS_ELM_DRIVES 2
-#define RT_DFS_ELM_MAX_SECTOR_SIZE 512
-#define RT_DFS_ELM_REENTRANT
 #define RT_USING_DFS_DEVFS
 
 /* Device Drivers */
@@ -109,7 +97,6 @@
 #define RT_USING_MTD_NOR
 #define RT_USING_SPI
 #define RT_USING_QSPI
-#define RT_USING_SPI_MSD
 #define RT_USING_SFUD
 #define RT_SFUD_USING_SFDP
 #define RT_SFUD_USING_FLASH_INFO_TABLE
@@ -253,6 +240,20 @@
 
 /* system packages */
 
+#define PKG_USING_FAL
+#define FAL_DEBUG_CONFIG
+#define FAL_DEBUG 1
+#define FAL_PART_HAS_TABLE_CFG
+#define FAL_USING_SFUD_PORT
+#define FAL_USING_NOR_FLASH_DEV_NAME "W25Q128"
+#define PKG_USING_FAL_LATEST_VERSION
+#define PKG_FAL_VER_NUM 0x99999
+#define PKG_USING_LITTLEFS
+#define PKG_USING_LITTLEFS_V170
+#define LFS_READ_SIZE 256
+#define LFS_PROG_SIZE 256
+#define LFS_BLOCK_SIZE 4096
+#define LFS_LOOKAHEAD 128
 
 /* peripheral libraries and drivers */
 
@@ -286,14 +287,13 @@
 /* Onboard Peripheral Drivers */
 
 #define BSP_USING_STLINK_TO_USART
-#define BSP_USING_SDCARD
+#define BSP_USING_QSPI_FLASH
 
 /* On-chip Peripheral Drivers */
 
 #define BSP_USING_GPIO
 #define BSP_USING_UART
 #define BSP_USING_UART1
-#define BSP_USING_ON_CHIP_FLASH
 #define BSP_USING_SPI
 #define BSP_USING_SPI1
 #define BSP_USING_SPI2
